a3c43b06f28e57f90f691241976bd56482680acc
Dénes Harmath <harmathdenes@gmail.com>
2018-08-25 15:27:38 +0200

    Github Pages integráció javítása

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 main=Anyag.md
 commonSettings="--toc --toc-depth=5 -V lang=hu"
 pandoc $main $commonSettings --pdf-engine=xelatex -V mainfont="FreeSerif"  -o Nyomtatás.pdf
-pandoc $main $commonSettings -t html -s -o Web.html
+pandoc $main $commonSettings -t html -s -o index.html
