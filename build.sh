6681c17f7ffaa8f186d1660d0b6b4ad144737c9d
Dénes Harmath <harmathdenes@gmail.com>
2018-12-25 21:33:38 +0100

    PDF verzió átnevezése

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 main=Anyag.md
 commonSettings="--toc --toc-depth=5 -V lang=hu"
-pandoc $main $commonSettings --pdf-engine=xelatex --filter pandoc-latex-color -V mainfont="FreeSerif" --include-in-header=preamble.latex -V colorlinks -o Nyomtatás.pdf
+pandoc $main $commonSettings --pdf-engine=xelatex --filter pandoc-latex-color -V mainfont="FreeSerif" --include-in-header=preamble.latex -V colorlinks -o "Szólalj meg bátran a zene nyelvén!.pdf"
 pandoc $main $commonSettings -t html -s -o index.html
